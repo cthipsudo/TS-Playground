480fc9bc29d0fd1ac4a69120e24ee9812a76539f
Chanpasong Thipphakhinkeo <Chanpasong.Thipphakhinkeo@gmail.com>
2026-04-14 18:23:22 -0400

    TS practice with pokemon

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,41 @@
 import './scripts/index.ts'
 import './App.css'
+import { useEffect, useState } from 'react';
+
+const fetchPokemon = async ():Promise<[{name:string, url:string}]> => {
+  const results = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=60&offset=60`);
+  if(!results.ok){
+    throw new Error(`Failed to fetch pokemon`);
+  }
+  const data = await results.json();
+  console.log(data);
+
+  return data.results;
+}
 
 function App() {
-  //const [count, setCount] = useState(0)
+const [pokemons, setPokemons] = useState<{name: string, url: string}[]>([]);
+
+useEffect(() => {
+  fetchPokemon().then(data => setPokemons(data));
+}, []);
+
 
   return (
     <>
-      
+      <div>
+        <ul>
+          {
+            pokemons.map((item, index) => {
+              return (
+                <li key={index}>
+                  {item.name}
+                </li>
+              )
+            }) 
+          }
+        </ul>
+      </div>
     </>
   )
 }
